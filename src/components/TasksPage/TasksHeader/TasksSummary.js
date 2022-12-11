8942e9cd8dfc060f2d56a7e71d8992051cbeb73b
SojerSM <seb.maz1996@gmail.com>
2022-12-11 10:29:03 +0100

    New task button added

--- src/components/TasksPage/TasksHeader/TasksSummary.js
+++ src/components/TasksPage/TasksHeader/TasksSummary.js
@@ -1,13 +1,23 @@
 import { Fragment } from "react";
 
+import * as icons from "../../../assets/icons";
 import styles from "./TasksSummary.module.css";
 
+import Button from "../../UI/Button";
 import SummaryButton from "./SummaryButton";
 
 const TasksSummary = function (props) {
   return (
     <Fragment>
-      <h3 className={styles.title}>Your Tasks</h3>
+      <div className={styles.header}>
+        <h3 className={styles.title}>Your Tasks</h3>
+        <Button className={styles["add-button"]}>
+          <svg className={styles["svg-icon"]} viewBox="0 0 20 20">
+            {icons.addNewIcon}
+          </svg>
+          Add
+        </Button>
+      </div>
       <div className={styles.summary}>
         <SummaryButton title={"All"} />
         <SummaryButton title={"Important"} />
