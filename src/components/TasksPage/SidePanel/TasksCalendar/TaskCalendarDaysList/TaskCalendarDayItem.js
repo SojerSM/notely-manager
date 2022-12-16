61b36c332eb11c2ec305737158fef55e20f8c8b3
SojerSM <seb.maz1996@gmail.com>
2022-12-16 17:26:33 +0100

    Filtering for tasks which dates matching clicked date item in calendar works

--- src/components/TasksPage/SidePanel/TasksCalendar/TaskCalendarDaysList/TaskCalendarDayItem.js
+++ src/components/TasksPage/SidePanel/TasksCalendar/TaskCalendarDaysList/TaskCalendarDayItem.js
@@ -1,19 +1,32 @@
+import { useContext } from "react";
+
 import styles from "./TaskCalendarDayItem.module.css";
 
+import TaskContext from "../../../../../store/taskContext/task-context";
+
 const TaskCalendarDayItem = function (props) {
+  const taskCtx = useContext(TaskContext);
+
   const dateHandler = (event) => {
     event.preventDefault();
     if (!props.date) return;
 
-    console.log(props.date);
+    /* Plus one hour because every task date is received with default hour 01:00 */
+
+    const matchingDates = taskCtx.tasks.filter((task) => {
+      return task.date.getTime() === props.date.getTime() + 3600000;
+    });
+
+    console.log(matchingDates);
   };
 
   return (
-    <button className={styles["wrapper"]} onClick={dateHandler}>
+    <div className={styles["wrapper"]} onClick={dateHandler}>
       <div className={`${styles["day"]} ${!props.day && styles["unactive"]}`}>
         <h5>{props.day}</h5>
+        <div className={`${props.day && styles["contain-panel"]}`}></div>
       </div>
-    </button>
+    </div>
   );
 };
 
