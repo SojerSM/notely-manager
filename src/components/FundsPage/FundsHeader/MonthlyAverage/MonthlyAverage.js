83549cce30ab251a6fee8403bc364b82f63e1336
SojerSM <seb.maz1996@gmail.com>
2022-12-21 17:45:56 +0100

    Monthly averages added as a separate component in funds header section

--- src/components/FundsPage/FundsHeader/MonthlyAverage/MonthlyAverage.js
+++ src/components/FundsPage/FundsHeader/MonthlyAverage/MonthlyAverage.js
@@ -1,12 +1,31 @@
+import { useContext } from "react";
+
 import styles from "./MonthlyAverage.module.css";
+
+import FundContext from "../../../../store/fundContext/fund-context";
 
 import CardFilled from "../../../UI/Cards/CardFilled";
 import FundCardHeader from "../../../UI/FundCardHeader";
 
 const MonthlyAverage = function (props) {
+  const { avgMonthExpensesValue, avgMonthIncomesValue } =
+    useContext(FundContext);
+
   return (
     <CardFilled className={styles["wrapper"]}>
       <FundCardHeader>{`This year average`}</FundCardHeader>
+      <div className={styles["averages"]}>
+        <div className={styles["averages-item"]}>
+          <p className={styles["title"]}>Incomes</p>
+          <p className={styles["amount"]}>{avgMonthIncomesValue}$</p>
+          <p className={styles["ending"]}>monthly</p>
+        </div>
+        <div className={styles["averages-item"]}>
+          <p className={styles["title"]}>Expenses</p>
+          <p className={styles["amount"]}>{avgMonthExpensesValue}$</p>
+          <p className={styles["ending"]}>monthly</p>
+        </div>
+      </div>
     </CardFilled>
   );
 };
