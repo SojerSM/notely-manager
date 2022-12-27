159bdb33c5ea4fa239c2a7b4c0c258c018970e6d
SojerSM <seb.maz1996@gmail.com>
2022-12-27 21:54:54 +0100

    Theme handler and context state toggler added

--- src/components/Navigation/NavButtons/ThemeButton.js
+++ src/components/Navigation/NavButtons/ThemeButton.js
@@ -1,9 +1,20 @@
+import { useContext } from "react";
+
 import styles from "./ThemeButton.module.css";
 import * as icons from "../../../assets/icons";
 
+import ThemeContext from "../../../store/themeContext/theme-context";
+
 const ThemeButton = function (props) {
+  const { toggleTheme } = useContext(ThemeContext);
+
+  const toggleThemeHandler = (event) => {
+    event.preventDefault();
+    toggleTheme();
+  };
+
   return (
-    <button className={styles["button"]}>
+    <button className={styles["button"]} onClick={toggleThemeHandler}>
       <svg className={styles["svg-icon"]} viewBox="0 0 20 20">
         {icons.themeIcon}
       </svg>
--- src/store/themeContext/ThemeProvider.js
+++ src/store/themeContext/ThemeProvider.js
@@ -6,13 +6,16 @@ const ThemeProvider = function (props) {
   const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
-    setTheme(null); // will be changed
+    theme === "dark" && setTheme("light");
+    theme === "light" && setTheme("dark");
   };
 
   const themeContext = {
     theme,
     toggleTheme,
   };
+
+  console.log(theme);
 
   return (
     <ThemeContext.Provider value={themeContext}>
