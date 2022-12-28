bb926a99a73ccdbeb56f26c184d30209a48c72e8
SojerSM <seb.maz1996@gmail.com>
2022-12-28 16:29:35 +0100

    Working on light mode: actions page dynamic styles ready

--- src/components/UI/Cards/Card.js
+++ src/components/UI/Cards/Card.js
@@ -1,10 +1,27 @@
-import React from "react";
+import { useContext, useState } from "react";
+
+import ThemeContext from "../../../store/themeContext/theme-context";
 
 import styles from "./Card.module.css";
 
 const Card = function (props) {
+  const { theme } = useContext(ThemeContext);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const bcgColor = `var(--${theme}__gray-light)`;
+
   return (
-    <div className={`${styles.card} ${props.className}`}>{props.children}</div>
+    <div
+      className={`${styles.card} ${props.className}`}
+      onMouseEnter={() => setIsFocused(true)}
+      onMouseLeave={() => setIsFocused(false)}
+      style={{
+        outline: `2px solid ${bcgColor}`,
+        backgroundColor: isFocused ? bcgColor : "",
+      }}
+    >
+      {props.children}
+    </div>
   );
 };
 
