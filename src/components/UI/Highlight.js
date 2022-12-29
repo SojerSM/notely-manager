d32f7fb281c3b4dde17f2a32859fe61fbbfe4a6c
SojerSM <seb.maz1996@gmail.com>
2022-12-29 17:07:08 +0100

    Working on light-mode: highlighted gradient is now dynamic

--- src/components/UI/Highlight.js
+++ src/components/UI/Highlight.js
@@ -1,7 +1,31 @@
+import { useContext } from "react";
+
 import styles from "./Highlight.module.css";
 
+import ThemeContext from "../../store/themeContext/theme-context";
+
 const Highlight = function (props) {
-  return <span className={styles["highlighted"]}>{props.children}</span>;
+  const { theme } = useContext(ThemeContext);
+
+  const gradient = {
+    color: theme === "dark" ? "rgba(225, 188, 40, 1)" : "rgba(255, 216, 36, 1)",
+    image:
+      theme === "dark"
+        ? `linear-gradient(115deg, rgba(225, 188, 40, 1) 0%, rgba(221, 128, 80, 1) 100%)`
+        : `linear-gradient(115deg, rgba(255, 216, 36, 1) 0%, rgba(196, 144, 22, 1) 100%)`,
+  };
+
+  return (
+    <span
+      className={styles["highlighted"]}
+      style={{
+        backgroundColor: gradient.color,
+        backgroundImage: gradient.image,
+      }}
+    >
+      {props.children}
+    </span>
+  );
 };
 
 export default Highlight;
